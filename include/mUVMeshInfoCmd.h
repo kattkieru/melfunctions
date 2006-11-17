3f670dc0def6ce2d5c27156dbf0314813bbf56bf
carsten.kolve <carsten.kolve@a6e4b3a4-6821-0410-b570-0dda8d89c68f>
2006-11-17 06:04:39 +0000

    added m2dShaderInfo, random commands, and added copyright infos

--- include/mUVMeshInfoCmd.h
+++ include/mUVMeshInfoCmd.h
@@ -1,3 +1,16 @@
+/* COPYRIGHT --
+ *
+ * This file is part of melfunctions, a collection of mel commands to for Autodesk Maya.
+ * melfunctions is (c) 2006 Carsten Kolve <carsten@kolve.com>
+ * and distributed under the terms of the GNU GPL V2.
+ * This contribution to melfunctions is (c) Rising Sun Pictures PTY Ltd, www.rsp.com.au 
+ * See the ./License-GPL.txt file in the source tree root for more information.
+ *
+ * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES
+ * OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
 //
 // cmd that allows querying of mesh parameters based on uv information
 //
